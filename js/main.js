9f0c25415ea070380ed77ccbfbf8cfceb57fe2e4
Ben Wyeth <benwyeth@192-168-1-4.tpgi.com.au>
2016-06-29 14:49:49 +1000

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function() {
+
+  $("#trumpet").click(function() {
+    var sound = document.getElementById("soundTrumpet");
+    sound.play()
+  });
+
+  $("#xylophone").click(function() {
+    var sound = document.getElementById("soundXylophone");
+    sound.play()
+  });
+
+  $("#bongo").click(function() {
+    var sound = document.getElementById("soundBongo");
+    sound.play()
+  });
+
+  $("#guitar").click(function() {
+    var sound = document.getElementById("soundGuitar");
+    sound.play()
+  });
+
+
+
+})
